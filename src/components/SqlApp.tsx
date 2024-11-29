3a75d3f63e0a287d9eef8afa0af8a6d96b2b1397
Bartłomiej Pawłowski <bpawlowski91@gmail.com>
2024-11-29 08:58:12 +0100

    Add: version 1.3 Split SQL Query into 3 TextFields, feature: select worksheet

--- src/components/SqlApp.tsx
+++ src/components/SqlApp.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 import '../App.css';
-import TextField from '@mui/material/TextField';
+import { FormControl, InputLabel, Select, MenuItem, TextField, Typography } from '@mui/material';
 import * as alasql from 'alasql';
 import * as XLSX from 'xlsx';
 import { MuiFileInput } from 'mui-file-input';
@@ -16,8 +16,15 @@ function SqlApp() {
   const { mode, setMode } = useThemeContext();
 
   const [data, setData] = useState({});
+
   const [alasqlQuery, setAlasqlQuery] = useState('');
-  const [inputValue, setInputValue] = useState('');
+  const [alasqlQueryBefore, setAlasqlQueryBefore] = useState('');
+  const [alasqlQuerySource, setAlasqlQuerySource] = useState('');
+  const [alasqlQueryAfter, setAlasqlQueryAfter] = useState('');
+
+  const [inputFileValue, setInputFileValue] = useState('');
+  const [availableWorkSheets, setAvailableWorkSheets] = useState([]);
+  const [currrentWorksheet, setCurrentWorksheet] = useState('');
 
   //execute AlaSQL query
   useEffect(() => {
@@ -33,6 +40,27 @@ function SqlApp() {
     }
   }, [alasqlQuery]);
 
+  //merge AlaSQL query @to be checked: potential trim issue
+  useEffect(() => {
+    if (alasqlQueryBefore || alasqlQuerySource || alasqlQueryAfter) {
+      setAlasqlQuery(alasqlQueryBefore + ' ' + alasqlQuerySource + ' ' + alasqlQueryAfter);
+    } else {
+      setAlasqlQuery(''); // Reset alasqlQuery when all inputs are empty
+    }
+  }, [alasqlQueryBefore, alasqlQuerySource, alasqlQueryAfter]);
+
+  //change current worksheet
+  useEffect(() => {
+    if (alasqlQuerySource) {
+      // Use a regular expression to find and replace the sheetid value
+      const updatedSource = alasqlQuerySource.replace(
+        /{sheetid: "(.*?)", autoExt: false}/,
+        `{sheetid: "${currrentWorksheet}", autoExt: false}`
+      );
+      setAlasqlQuerySource(updatedSource);
+    }
+  }, [currrentWorksheet]);
+
   const handleFileChange = (newInputValue) => {
     const file = newInputValue;
     const fileName = file.name;
@@ -44,17 +72,23 @@ function SqlApp() {
       reader.onload = (e) => {
         const data = e.target.result; // Binary string or array buffer
         let workbook = XLSX.read(data, { type: 'binary' });
+
+        console.log(workbook.SheetNames);
+        setAvailableWorkSheets(() => workbook.SheetNames);
+        setCurrentWorksheet(() => workbook.SheetNames[workbook.SheetNames.length - 1]);  // default current worksheet - the last one
   
         // Store temporary path for AlaSQL
         let tmppath = URL.createObjectURL(file);
   
-        setInputValue(() => file);
-        setAlasqlQuery(`SELECT * FROM ${fileExtension}("${tmppath}", {sheetid: "${workbook.SheetNames[workbook.SheetNames.length - 1]}", autoExt: false})`);
+        setInputFileValue(() => file);
+        setAlasqlQueryBefore('SELECT *');
+        setAlasqlQuerySource(`FROM ${fileExtension}("${tmppath}", {sheetid: "${currrentWorksheet}", autoExt: false})`);
+        setAlasqlQueryAfter('');
       };
   
       reader.readAsBinaryString(file); // Read file as binary string
     } else {
-      setInputValue('');
+      setInputFileValue('');
       alert('Invalid file type! Please upload a CSV, XLS, or XLSX file.');
     }
   };
@@ -62,30 +96,84 @@ function SqlApp() {
     return ( 
         <>
 
-        <DarkModeSwitch checked={mode === 'dark'} onChange={() => setMode(mode === 'dark' ? 'light' : 'dark')} size={24} sunColor='currentColor' moonColor='currentColor'
-        style={{position: 'absolute', top: '17px', right: '17px'}}/>
+      <DarkModeSwitch checked={mode === 'dark'} onChange={() => setMode(mode === 'dark' ? 'light' : 'dark')} size={24} sunColor='currentColor' moonColor='currentColor'
+      style={{position: 'absolute', top: '17px', right: '17px'}}/>
 
-        <MuiFileInput
-      label="Wybierz plik CSV/XLS/XLSX"
-      accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-      value={inputValue}
-      onChange={handleFileChange}
-      sx={{
-        mb: 2,
-      }} />
+      <MuiFileInput
+        label="Wybierz plik CSV/XLS/XLSX"
+        accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+        value={inputFileValue}
+        onChange={handleFileChange}
+        sx={{
+          mb: 2,
+        }} 
+      />
 
-    <TextField 
+      {availableWorkSheets.length > 0 && currrentWorksheet && (
+        <FormControl sx={{ mb: 2, ml: 1, minWidth: 150 }}>
+          <InputLabel id="select-worksheet-label">Arkusz</InputLabel>
+          <Select
+            labelId="select-worksheet-label"
+            id="select-worksheet"
+            value={currrentWorksheet}
+            label="Arkusz"
+            onChange={(e) => setCurrentWorksheet(e.target.value)}
+          >
+            {availableWorkSheets.map((worksheet) => (
+              <MenuItem key={worksheet} value={worksheet}>
+                {worksheet}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      )}
+
+      <TextField 
+        // disabled={!inputFileValue}
         fullWidth
         autoComplete='off'
         id="outlined-basic"
-        label="SQL"
+        label="SQL Before"
         variant="outlined"
-        value={alasqlQuery}
-        onChange={(e) => setAlasqlQuery(e.target.value)}
+        value={alasqlQueryBefore}
+        onChange={(e) => setAlasqlQueryBefore(e.target.value)}
         sx={{
           mb: 2,
         }}
       />
+      <TextField 
+        disabled
+        autoComplete='off'
+        id="outlined-basic"
+        label="SQL Source"
+        variant="outlined"
+        value={alasqlQuerySource}
+        onChange={(e) => setAlasqlQuerySource(e.target.value)}
+        sx={{
+          mb: 2,
+        }}
+      />
+
+      <TextField 
+        // disabled={!inputFileValue}
+        fullWidth
+        autoComplete='off'
+        id="outlined-basic"
+        label="SQL After"
+        variant="outlined"
+        value={alasqlQueryAfter}
+        onChange={(e) => setAlasqlQueryAfter(e.target.value)}
+        sx={{
+          mb: 2,
+        }}
+      />
+
+      {/* {alasqlQuery && 
+        <>
+            <Typography variant="h6">Final SQL Query: </Typography>
+            <Typography variant="h6" sx={{mb: 2}}>{alasqlQuery}</Typography>
+        </>
+      } */}
 
       <table className="table table-striped">
         <thead>
